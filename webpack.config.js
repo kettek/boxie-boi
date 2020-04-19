c478366436e1e1962ed1e46abb3cdf2957dcdb0a
kts of kettek (POWERQWACK) <kts@kettek.net>
2020-04-18 23:04:01 -0700

    Adjust fonts locations

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,18 @@ module.exports = {
         test: /\.css$/,
         //use: [CSSExtractPlugin.loader, 'style-loader', 'css-loader']
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ]
   },
